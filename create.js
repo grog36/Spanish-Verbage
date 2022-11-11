fae7adb827e00638b66847aa9748068d52b51069
grog36 <gregistigger@gmail.com>
2022-11-11 13:50:05 -0500

    Updated verbs.json and began conjugations

--- create.js
+++ create.js
@@ -1,181 +1,102 @@
 let VERBS = require("./verbs.json");
-let normalVerbList = [
-    "abrir",
-    "ayudar",
-    "bailar",
-    "beber",
-    "buscar",
-    "cambiar",
-    "caminar",
-    "cantar",
-    "cenar",
-    "comer",
-    "comprar",
-    "comprender",
-    "cortar",
-    "creer",
-    "dibujar",
-    "disfrutar",
-    "escribir",
-    "escuchar",
-    "esperar",
-    "estudiar",
-    "explicar",
-    "formar",
-    "ganar",
-    "gustar",
-    "hablar",
-    "invitar",
-    "lavar",
-    "leer",
-    "limpiar",
-    "llamar",
-    "llegar",
-    "llevar",
-    "manejar",
-    "mirar",
-    "montar",
-    "nadar",
-    "pagar",
-    "pasar",
-    "pintar",
-    "preguntar",
-    "presentar",
-    "romper",
-    "saltar",
-    "trabajar",
-    "viajar",
-    "vivir"
-]
 const fs = require("fs");
-// for (let verb of normalVerbList) {
-//     if (!VERBS[verb]["isReflexive"]) {
-//         let root = "";
-//         let type = verb[verb.length - 2];
-//         for (let letterIndex in verb) {
-//             if (letterIndex != verb.length - 1 && letterIndex != verb.length - 2) {
-//                 root += verb[letterIndex];
-//             }
-//         }
-
-//         if (type == "a") {
-//             VERBS[verb]["indicativeConjugations"] = {
-//                 "presentTense": {
-//                     "firstPersonSingular": (root + "o"),
-//                     "secondPersonSingular": (root + "as"),
-//                     "thirdPersonSingular": (root + "a"),
-//                     "firstPersonPlural": (root + "amos"),
-//                     "secondPersonPlural": (root + "áis"),
-//                     "thirdPersonPlural": (root + "an")
-//                 }
-//             }
-//         }
-
-//         else if (type == "i") {
-//             VERBS[verb]["indicativeConjugations"] = {
-//                 "presentTense": {
-//                     "firstPersonSingular": (root + o),
-//                     "secondPersonSingular": (root + "es"),
-//                     "thirdPersonSingular": (root + "e"),
-//                     "firstPersonPlural": (root + "imos"),
-//                     "secondPersonSingular": (root + "ís"),
-//                     "thirdPersonSingular": (root + "en")
-//                 }
-//             }
-//         }
-
-//         else if (type == "e") {
-
-//         }
-
-//         else {
-//             console.log("ERROR!\nWORD: " + verb);
-//         }
-
-//     }
-// }
-
-//fs.writeFile("./temp.json", JSON.stringify(VERBS, null, 2), (err) => {
-//    if (err) throw err;
-//});
-
-
-function assignNormalConjugationsIndicativeTense(word, tense) {
-    if (VERBS[word]["isReflexive"] == false) {
-        let root = word.slice(0, word.length - 2);
-        let ending = word.slice(word.length - 2, word.length);
-
-        switch (tense) {
-            case "presentTense":
-                
-                //HERE
-
-
-
-
-
-
-
-
-                break;
-            case "preteriteTense":
-                console.log("Pret");
-                break;
-            case "imperfectTense":
-                console.log("Imp");
-                break;
-            case "conditionalTense":
-                console.log("Cond");
-                break;
-            case "futureTense":
-                console.log("Fut");
-                break;
-            default:
-                console.log("ERROR");
-                break;
-        }
-
-    }
-    else {
-        console.log(`${word} is reflexive. Unable to conjugate yet!`);
-    }
-}
-
-
-
-function assignAllNormalConjugationsIndicativeTense(word) {
-    assignNormalConjugationsIndicativeTense(word, "presentTense");
-    assignNormalConjugationsIndicativeTense(word, "preteriteTense");
-    assignNormalConjugationsIndicativeTense(word, "imperfectTense");
-    assignNormalConjugationsIndicativeTense(word, "conditionalTense");
-    assignNormalConjugationsIndicativeTense(word, "futureTense");
-}
-
-
-
-
-function initialize(verb) {
-    VERBS[verb]["indicativeConjugations"] = {};
-
-    let tenses = ["presentTense", "preteriteTense", "imperfectTense", "conditionalTense", "futureTense"];
-
-
-    for (let tense of tenses) {
-        VERBS[verb]["indicativeConjugations"][tense] = {
-            "firstPersonSingular": null,
-            "secondPersonSingular": null,
-            "thirdPersonSingular": null,
-            "firstPersonPlural": null,
-            "secondPersonPlural": null,
-            "thirdPersonPlural": null
-        }
-    }
-}
-
-
 for (let verb in VERBS) {
-    initialize(verb);
+    let root = verb.slice(0, verb.length - 2);
+    let ending= verb.slice(verb.length -2, verb.length);
+    let basePresent = VERBS[verb]["indicativeConjugations"]["presentTense"];
+    let basePreterite = VERBS[verb]["indicativeConjugations"]["preteriteTense"];
+    let baseImperfect = VERBS[verb]["indicativeConjugations"]["imperfectTense"];
+    let baseConditional = VERBS[verb]["indicativeConjugations"]["conditionalTense"];
+    let baseFuture = VERBS[verb]["indicativeConjugations"]["futureTense"];
+
+    if (basePresent["isRegular"]) {
+        if (ending === "ar") {
+            basePresent["firstPersonSingular"] = root + "o";
+            basePresent["secondPersonSingular"] = root + "as";
+            basePresent["thirdPersonSingular"] = root + "a";
+            basePresent["firstPersonPlural"] = root + "amos";
+            basePresent["secondPersonPlural"] = root + "áis";
+            basePresent["thirdPersonPlural"] = root + "an";
+        }
+        else if (ending === "er") {
+            basePresent["firstPersonSingular"] = root + "o";
+            basePresent["secondPersonSingular"] = root + "es";
+            basePresent["thirdPersonSingular"] = root + "e";
+            basePresent["firstPersonPlural"] = root + "emos";
+            basePresent["secondPersonPlural"] = root + "éis";
+            basePresent["thirdPersonPlural"] = root + "en";
+        }
+        else if (ending === "ir") {
+            basePresent["firstPersonSingular"] = root + "o";
+            basePresent["secondPersonSingular"] = root + "es";
+            basePresent["thirdPersonSingular"] = root + "e";
+            basePresent["firstPersonPlural"] = root + "imos";
+            basePresent["secondPersonPlural"] = root + "ís";
+            basePresent["thirdPersonPlural"] = root + "en";
+        }
+    }
+
+    if (basePreterite["isRegular"]) {
+        if (ending === "ar") {
+            basePreterite["firstPersonSingular"] = root + "é";
+            basePreterite["secondPersonSingular"] = root + "aste";
+            basePreterite["thirdPersonSingular"] = root + "ó";
+            basePreterite["firstPersonPlural"] = root + "amos";
+            basePreterite["secondPersonPlural"] = root + "asteis";
+            basePreterite["thirdPersonPlural"] = root + "aron";
+        }
+        else if (ending === "er" || ending === "ir") {
+            basePreterite["firstPersonSingular"] = root + "í";
+            basePreterite["secondPersonSingular"] = root + "iste";
+            basePreterite["thirdPersonSingular"] = root + "ió";
+            basePreterite["firstPersonPlural"] = root + "imos";
+            basePreterite["secondPersonPlural"] = root + "isteis";
+            basePreterite["thirdPersonPlural"] = root + "ieron";
+        }
+    }
+
+    if (baseImperfect["isRegular"]) {
+        if (ending === "ar") {
+            baseImperfect["firstPersonSingular"] = root + "aba";
+            baseImperfect["secondPersonSingular"] = root + "abas";
+            baseImperfect["thirdPersonSingular"] = root + "aba";
+            baseImperfect["firstPersonPlural"] = root + "ábamos";
+            baseImperfect["secondPersonPlural"] = root + "abais";
+            baseImperfect["thirdPersonPlural"] = root + "aban";
+        }
+        else if (ending === "er" || ending === "ir") {
+            baseImperfect["firstPersonSingular"] = root + "ía";
+            baseImperfect["secondPersonSingular"] = root + "ías";
+            baseImperfect["thirdPersonSingular"] = root + "ía";
+            baseImperfect["firstPersonPlural"] = root + "íamos";
+            baseImperfect["secondPersonPlural"] = root + "íais";
+            baseImperfect["thirdPersonPlural"] = root + "ían";
+        }
+    }
+
+    if (baseConditional["isRegular"]) {
+        if (ending === "ar" || ending === "er" || ending === "ir") {
+            baseConditional["firstPersonSingular"] = verb + "ía";
+            baseConditional["secondPersonSingular"] = verb + "ías";
+            baseConditional["thirdPersonSingular"] = verb + "ía";
+            baseConditional["firstPersonPlural"] = verb + "íamos";
+            baseConditional["secondPersonPlural"] = verb + "íais";
+            baseConditional["thirdPersonPlural"] = verb + "ían";
+        }
+    }
+
+    if (baseFuture["isRegular"]) {
+        if (ending === "ar" || ending === "er" || ending === "ir") {
+            baseFuture["firstPersonSingular"] = verb + "é";
+            baseFuture["secondPersonSingular"] = verb + "ás";
+            baseFuture["thirdPersonSingular"] = verb + "á";
+            baseFuture["firstPersonPlural"] = verb + "emos";
+            baseFuture["secondPersonPlural"] = verb + "éis";
+            baseFuture["thirdPersonPlural"] = verb + "án";
+        }
+    }
 }
+
 
 fs.writeFile("./out.json", JSON.stringify(VERBS, null, 2), err => {
     if (err) throw err;
